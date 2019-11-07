2f297249c784b98f1e709daf940791638ce6bb34
Shahwar Spall <shevy.spall@gmail.com>
2019-11-07 20:14:39 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,9 @@
-levitation_force = 10 
+levitation_force = 0
 
 def using_until
   
   until levitation_force == 10
   puts "Wingardium Leviosa"
-    levitation_force += 1
+  levitation_force += 1
 end
 
